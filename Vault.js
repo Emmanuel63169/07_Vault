11e69317adbb5c9e3e143ae7eda1dc9adbef6519
Emmanuel63169 <emmanuel63169@gmail.com>
2024-11-15 19:23:05 -0700

    Vault.css

---
+++ Vault.js
@@ -0,0 +1,38 @@
+// Here is the JavaScript for my vault!
+
+// String -_-_-_
+
+let dash = "-";
+let space = " ";
+let str1 = "You";
+let str2 = "have";
+let str3 = "recieved";
+let str4 = "this";
+let str5 = "message";
+let str6 = "because";
+let str7 = "been";
+let str8 = "chosen";
+let str9 = "to";
+let str10 = "open";
+let str11 = "an";
+let str12 = "important";
+let str13 = "vault.";
+let str14 = "Here";
+let str15 = "is";
+let str16 = "the";
+let str17 = "secret";
+let str18 = "convination:";
+
+let fullstring = str1 + space + str2 + space + str3 + space + str4 + space + str6 + space + str1 + space + str2 + space + str7 + space + str8 + space + str9 + space + str10 + space + str11 + space + str12 + space + str13 + space + str14 + space + str15 + space + str16 + space + str17 + space + str18
+console.log(fullstring);
+
+const num1 = 5 + 5;
+const num2 = 100 - 60;
+const num3 = 13 * 3;
+
+// Alert -_-_-_
+
+alert(num1 + dash + num2 + dash + num3)
+
+// End of the Javascript -_-_-_
+  
